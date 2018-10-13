676a04b8a9eabe878255a6166f50a76e972d7926
James Webber <James.Webber@cansfordlabs.co.uk>
2018-10-13 21:02:24 +0100

    Ex1

--- BasicAlgorithms/Program.cs
+++ BasicAlgorithms/Program.cs
@@ -8,8 +8,19 @@ namespace BasicAlgorithms
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // 1. Write a C# Sharp program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
+            Console.WriteLine(Ex1(1, 2));
+            Console.WriteLine(Ex1(2, 2));
+
+
+            Console.ReadLine();
+        }
+
+        private static int Ex1(int n1, int n2)
+        {
+            return n1 == n2? (n1 + n2) * 3: n1 + n2;
         }
     }
 }
