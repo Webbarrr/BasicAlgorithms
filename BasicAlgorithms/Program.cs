0096441efa310d6e42de73ed6456e53006cbcd52
James Webber <James.Webber@cansfordlabs.co.uk>
2018-10-14 21:44:15 +0100

    Ex6-20

--- BasicAlgorithms/Program.cs
+++ BasicAlgorithms/Program.cs
@@ -30,9 +30,95 @@ namespace BasicAlgorithms
             //Console.WriteLine(Ex4(90));
             //Console.WriteLine(Ex4(89));
 
-            // 5. Write a C# Sharp program to create a new string where 'if' is added to the front of a given string. If the string already begins with 'if', return the string unchanged.
-            Console.WriteLine(Ex5("if else"));
-            Console.WriteLine(Ex5("else"));
+            //// 5. Write a C# Sharp program to create a new string where 'if' is added to the front of a given string. If the string already begins with 'if', return the string unchanged.
+            //Console.WriteLine(Ex5("if else"));
+            //Console.WriteLine(Ex5("else"));
+
+            //// 6. Write a C# Sharp program to remove the character in a given position of a given string. The given position will be in the range 0.. string length -1 inclusive.
+            //Console.WriteLine(Ex6("Python", 1));
+            //Console.WriteLine(Ex6("Python", 0));
+            //Console.WriteLine(Ex6("Python", 4));
+
+            //// 7. Write a C# Sharp program to exchange the first and last characters in a given string and return the new string.
+            //Console.WriteLine(Ex7("abcd"));
+            //Console.WriteLine(Ex7("a"));
+            //Console.WriteLine(Ex7("xy"));
+
+            //// 8. Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string.
+            //// If the given string length is less than 2 return the original string.
+            //Console.WriteLine(Ex8("C Sharp"));
+            //Console.WriteLine(Ex8("JS"));
+            //Console.WriteLine(Ex8("a"));
+
+            //// 9. Write a C# Sharp program to create a new string with the last char added at the front and back of a given string of length 1 or more.
+            //Console.WriteLine(Ex9("Red"));
+            //Console.WriteLine(Ex9("Green"));
+            //Console.WriteLine(Ex9("1"));
+
+            //// 10. Write a C# Sharp program to check if a given positive number is a multiple of 3 or a multiple of 7.
+            //Console.WriteLine(Ex10(3));
+            //Console.WriteLine(Ex10(14));
+            //Console.WriteLine(Ex10(12));
+            //Console.WriteLine(Ex10(37));
+
+            //// 11. Write a C# Sharp program to create a new string taking the first 3 characters of a given string and return the string with the 3 characters added at 
+            //// both the front and back. If the given string length is less than 3, use whatever characters are there.
+            //Console.WriteLine(Ex11("Python"));
+            //Console.WriteLine(Ex11("JS"));
+            //Console.WriteLine(Ex11("Code"));
+
+            //// 12. Write a C# Sharp program to check if a given string starts with 'C#' or not.
+            //Console.WriteLine(Ex12("C# Sharp"));
+            //Console.WriteLine(Ex12("C#"));
+            //Console.WriteLine(Ex12("C++"));
+
+            //// 13. Write a C# Sharp program to check if one given temperatures is less than 0 and the other is greater than 100.
+            //Console.WriteLine(Ex13(120, -1));
+            //Console.WriteLine(Ex13(-1, 120));
+            //Console.WriteLine(Ex13(2, 120));
+
+            //// 14. Write a C# Sharp program to check two given integers whether either of them is in the range 100..200 inclusive.
+            //Console.WriteLine(Ex14(100, 199));
+            //Console.WriteLine(Ex14(250, 300));
+            //Console.WriteLine(Ex14(105, 190));
+
+            //// 15. Write a C# Sharp program to check whether three given integer values are in the range 20..50 inclusive.
+            //// Return true if 1 or more of them are in the said range otherwise false.
+            //Console.WriteLine(Ex15(11, 20, 12));
+            //Console.WriteLine(Ex15(30, 30, 17));
+            //Console.WriteLine(Ex15(25, 35, 50));
+            //Console.WriteLine(Ex15(15, 12, 8));
+
+            //// 16. Write a C# Sharp program to check whether two given integer values are in the range 20..50 inclusive. 
+            //// Return true if 1 or other is in the said range otherwise false.
+            //Console.WriteLine(Ex16(20, 84));
+            //Console.WriteLine(Ex16(14, 50));
+            //Console.WriteLine(Ex16(11, 45));
+            //Console.WriteLine(Ex16(25, 40));
+            //Console.WriteLine(Ex16(56, 57));
+
+            //// 17. Write a C# Sharp program to check if a string 'yt' appears at index 1 in a given string. If it appears return a string without 'yt' 
+            //// otherwise return the original string.
+            //Console.WriteLine(Ex17("Python"));
+            //Console.WriteLine(Ex17("ytade"));
+            //Console.WriteLine(Ex17("jsues"));
+
+            //// 18. Write a C# Sharp program to check the largest number among three given integers.
+            //Console.WriteLine(Ex18(1, 2, 3));
+            //Console.WriteLine(Ex18(1, 3, 2));
+            //Console.WriteLine(Ex18(1, 1, 1));
+            //Console.WriteLine(Ex18(1, 2, 2));
+
+            //// 19. Write a C# Sharp program to check which number nearest to the value 100 among two given integers. Return 0 if the two numbers are equal.
+            //Console.WriteLine(Ex19(78, 95));
+            //Console.WriteLine(Ex19(95, 95));
+            //Console.WriteLine(Ex19(99, 70));
+
+            // 20. Write a C# Sharp program to check whether two given integers are in the range 40..50 inclusive, or they are both in the range 50..60 inclusive.
+            Console.WriteLine(Ex20(78, 95));
+            Console.WriteLine(Ex20(25, 35));
+            Console.WriteLine(Ex20(40, 50));
+            Console.WriteLine(Ex20(55, 60));
 
 
             Console.ReadLine();
@@ -59,6 +145,104 @@ namespace BasicAlgorithms
         private static string Ex5(string yourString)
         {
             return yourString.ToLower().StartsWith("if") ? yourString : "if " + yourString;
+        }
+        private static string Ex6(string yourString, int position)
+        {
+            return yourString.Remove(position,1);
+        }
+        private static string Ex7(string yourString)
+        {
+            char[] letters = yourString.ToCharArray();
+            int size = letters.Length - 1;
+
+            StringBuilder retVal = new StringBuilder();
+            retVal.Append(letters[size]);
+
+            for (int i = 1; i < size; i++) retVal.Append(letters[i]);
+            retVal.Append(letters[0]);
+
+            return string.Join(null, retVal);
+        }
+        public static string Ex7Better(string str)
+        {
+            return str.Length > 1 ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
+        }
+        public static string Ex8(string yourString)
+        {
+            StringBuilder retVal = new StringBuilder();
+            if (yourString.Length < 2) return yourString;
+            for (int i = 0; i < 4; i++) retVal.Append(yourString.Substring(0, 2));
+            return retVal.ToString();
+        }
+        public static string Ex9(string yourString)
+        {
+            string letter = yourString.Substring(yourString.Length - 1);
+            return yourString.Length < 1 ? yourString
+                : letter + yourString + letter;
+        }
+        public static bool Ex10(int n)
+        {
+            if (n < 3) return false;
+            return n % 3 == 0 || n % 7 == 0 ? true: false;
+        }
+        private static string Ex11(string yourString)
+        {
+            int size = 3;
+            StringBuilder retVal = new StringBuilder();
+            if ( yourString.Length < size) for (int i = 0; i < 2; i++) retVal.Append(yourString);
+            else
+            {
+                string letters = yourString.Substring(0, size);
+                retVal.Append(letters);
+                retVal.Append(yourString);
+                retVal.Append(letters);
+            }
+            return retVal.ToString();
+        }
+        private static bool Ex12(string yourString,string startsWith = "C#")
+        {
+            return yourString.ToLower().StartsWith(startsWith.ToLower()) ? true : false;
+        }
+        private static bool Ex13(int temp1, int temp2)
+        {
+            int min = 0, max = 100;
+            return (temp1 < min && temp2 > max) || (temp2 < min && temp1 > max) ? true : false;
+        }
+        private static bool Ex14(int n1, int n2)
+        {
+            int min = 100, max = 200;
+            return (n1 >= min && n1 <= max) || (n2 >= min && n2 <= max) ? true : false;
+        }
+        private static bool Ex15(int n1, int n2, int n3)
+        {
+            int min = 20, max = 50;
+            return (n1 >= min && n1 <= max) || (n2 >= min && n2 <= max) || (n3 >= min && n3 <= max) ? true : false;
+        }
+        private static bool Ex16(int n1, int n2)
+        {
+            int min = 20, max = 50;
+            return (n1 >= min && n2 <= max) || (n2 >= min && n1 <= max);
+        }
+        private static string Ex17(string yourString)
+        {
+            return yourString.Substring(1, 2).ToString().ToLower() == "yt" ? yourString.Remove(1, 2) : yourString;
+        }
+        private static int Ex18(int n1, int n2, int n3)
+        {
+            return Math.Max(Math.Max(n1, n2), n3);
+        }
+        private static int Ex19(int n1, int n2)
+        {
+            if (n1 == n2) return 0;
+            int max = 100;
+            return Math.Abs(n1 - max) < Math.Abs(n2 - max) ? n1 : n2;
+        }
+        private static bool Ex20(int n1, int n2)
+        {
+            int lower1 = 40, lower2 = 50, increment = 10;
+            return (n1 >= lower1 && n2 <= lower1 + increment) || (n2 >= lower1 && n1 <= lower1 + increment)
+                || (n1 >= lower2 && n2 <= lower2 + increment) || (n2 >= lower2 && n1 <= lower2 + increment)
+                ? true : false;
         }
     }
 }
